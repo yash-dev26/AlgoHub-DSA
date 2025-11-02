01e6c0cc60596eafe85e60795d3120f49c34c171
Yashwardhan Singh <yash.pvt2601@gmail.com>
2025-11-02 15:47:23 +0530

    added status codes using http-status-codes library

--- AlgoHubBackend/AlgoHub-Problem-Service/src/controllers/problem.controller.js
+++ AlgoHubBackend/AlgoHub-Problem-Service/src/controllers/problem.controller.js
@@ -1,25 +1,37 @@
+const { StatusCodes } = require('http-status-codes');   
+
 function pingProblemController(req, res){
     return res.json({"message": "Pong from Problem Controller"});
 }
 
 function addProblem(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: "Add Problem not implemented"
+    });
 }
 
 function getProblem(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: "Get Problem not implemented"
+    });
 }
 
 function getProblems(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: "Get Problems not implemented"
+    });
 }
 
 function deleteProblem(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: "Delete Problem not implemented"
+    });
 }
 
 function updateProblem(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: "Update Problem not implemented"
+    });
 }
 
 module.exports = {
