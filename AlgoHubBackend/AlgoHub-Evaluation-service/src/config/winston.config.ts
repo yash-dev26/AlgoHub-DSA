ca432ba0986802046fc9bfa2e34ed807bdbdf215
Yashwardhan Singh <yash.pvt2601@gmail.com>
2025-11-28 16:59:47 +0530

    feat(eval): add winston config setup

---
+++ AlgoHubBackend/AlgoHub-Evaluation-service/src/config/winston.config.ts
@@ -0,0 +1,33 @@
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const allowedTransports = [];
+
+allowedTransports.push(
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize({ all: true }),
+      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      winston.format.printf((info) => `${info.timestamp} [${info.level}]: ${info.message}`),
+    ),
+  }),
+);
+
+allowedTransports.push(
+  new DailyRotateFile({
+    filename: 'logs/app-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    maxSize: '15m', //maximum size of a log file is 15mb
+    maxFiles: '7d', //keep logs for 7 days atmax
+  }),
+);
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf((info) => `${info.timestamp} [${info.level}]: ${info.message}`),
+  ),
+  transports: allowedTransports,
+});
+
+export default logger;
