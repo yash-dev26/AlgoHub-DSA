8d1860de2878b04ee938a44ffc7193590ad921bf
Yashwardhan Singh <yash.pvt2601@gmail.com>
2026-02-23 12:42:45 +0530

    fix: removed irrelevant comments to make the code cleaner

--- AlgoHubBackend/AlgoHub-Evaluation-service/src/index.ts
+++ AlgoHubBackend/AlgoHub-Evaluation-service/src/index.ts
@@ -1,15 +1,11 @@
 import express, { Express } from 'express';
 import serverConfig from './config/server.config';
 import apirouter from './routes';
-// import SampleProducer from './producer/Sample.producer';
-// import SampleWorker from './consumer/sample.consumer';
 import logger from './config/winston.config';
 import bullAdapter from './config/bullBoard.config';
 import bodyParser from 'body-parser';
-// import runCppCode from './container/cpp.container';
 import { Submission_Queue_Name } from './utils/constants';
 import SubmissionWorker from './consumer/submission.consumer';
-import submissionProducer from './producer/submission.producer';
 
 const app: Express = express();
 
@@ -26,36 +22,5 @@ app.listen(serverConfig.PORT, () => {
     source: 'index.ts',
   });
 
-  // // SampleWorker('SampleQueue');
-  // const code = `
-  // #include <iostream>
-  // using namespace std;
-  // int main() {
-  //   int n;
-  //   cin >> n;
-  //   cout << n * 2 << endl;
-  //   for(int i = 0; i < n; i++) {
-  //       cout << "Hello, World!" << endl;
-  //   }
-  //   return 0;
-  // }
-  // `;
-
   SubmissionWorker(Submission_Queue_Name);
-  // submissionProducer({
-  //   '1234': {
-  //     language: 'CPP',
-  //     code,
-  //     input: '5',
-  //   },
-  // });
-
-  // SampleProducer('SampleJob', {
-  //   name: 'TestPayload',
-  //   value: 42,
-  //   location: 'TestLocation',
-  //   language: 'js',
-  // });
-  //
-  //   runCppCode(code, { input: '8', output: '' });
 });
